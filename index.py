b6d51a0ac016cc70b63044655f8d2f082888df35
Edwin Laurent <laurent.edwin@icloud.com>
2023-02-24 10:53:46 +0100

    wip - get all books datas from one categorie

--- index.py
+++ index.py
@@ -6,10 +6,10 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 #GET url request 
-url = 'http://books.toscrape.com/'
+# url = 'http://books.toscrape.com/'
 
-response =requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+# response =requests.get(url)
+# soup = BeautifulSoup(response.text, 'html.parser')
 
 #Get href from category list (from left vertical menu)
 def get_categories_links() :
@@ -22,12 +22,10 @@ def get_categories_links() :
             href = href.replace('index.html','')
             categories_link_list.append(href)
     return categories_link_list
-
 #categories_list = get_categories_links()
 
 
 #Get all the pages of a specific category
-
 #### V1  Loop to get all the links of the website
 # def get_category_pages(): 
 #     category_pages = []
@@ -64,9 +62,8 @@ def get_category_pages(category_path):
             page = 1
             break
     return specific_category_pages
-
 category_pages = get_category_pages('catalogue/category/books/historical-fiction_4/')
-print(category_pages)
+
 
 
 #Get all articles links from category pages
@@ -87,64 +84,68 @@ def get_articles_links() :
                 base_url = "https://books.toscrape.com/"
                 href = href.replace('../../../','')
                 full_article_link = urljoin(('https://books.toscrape.com/catalogue/'), str(href))
-                print(full_article_link)
                 articles_link_list.append(full_article_link)
     return articles_link_list
-    
-test = get_articles_links()
-print(test)
+articles_links = get_articles_links()
 
 
-def get_book_informations():
-    for article in soup.findAll('article'):
-    #Title
+
+
+def get_book_informations(book_url):
+    url = book_url
+    response =requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    book_data = []
+    if response.ok : 
+        article = soup.find('article')
+        #Title
         product_title = article.select('.product_main h1')[0].get_text()
 
-    #Product page URL
+        #Product page URL
         page_url = response.url
-    
-    #Price including taxes
+
+        #Price including taxes
         th_product_price_incl_taxes = soup.find('th', text ='Price (incl. tax)')
         td_product_price_incl_taxes = th_product_price_incl_taxes.find_next_sibling('td')
-    
-    #Price excluding taxes 
+
+        #Price excluding taxes 
         th_product_price_excl_taxes = soup.find('th', text ='Price (excl. tax)')
         td_product_price_excl_taxes = th_product_price_excl_taxes.find_next_sibling('td')
 
-    #Number available
+        #Number available
         th_product_availability = soup.find('th', text ='Availability')
         td_product_stock = th_product_availability.find_next_sibling('td').get_text()
         product_stock_quantity = int(re.findall(r'\d+', td_product_stock)[0])
 
-    #Product description
+        #Product description
         product_description_title = soup.find('div', {'id':'product_description'})
         product_description = product_description_title.find_next_sibling('p')
 
-    #Category
+        #Category
         th_product_type = soup.find('th', text ='Product Type')
         td_product_category = th_product_type.find_next_sibling('td')
 
-    #Review rating
+        #Review rating
         product_rating_class = soup.find(class_='star-rating')
         product_rating_value_string = product_rating_class.get('class')[1]
-        
+
         help_dict = {
-            'Zero': '0',
-            'One': '1',
-            'Two': '2',
-            'Three': '3',
-            'Four': '4',
-            'Five': '5'
+        'Zero': '0',
+        'One': '1',
+        'Two': '2',
+        'Three': '3',
+        'Four': '4',
+        'Five': '5'
         }
 
         product_rating_value = help_dict[product_rating_value_string] 
 
-    #UPC
+        #UPC
         th_product_upc = soup.find('th', text ='UPC')
         td_product_upc = th_product_upc.find_next_sibling('td')
         th_product_upc_text = soup.find('th', text ='UPC').get_text()
         td_product_upc_text = th_product_upc.find_next_sibling('td').get_text()
-    #Image URL 
+        #Image URL 
 
         base_url = "https://books.toscrape.com"
 
@@ -152,19 +153,38 @@ def get_book_informations():
         product_image = gallery_div.find('img')
         product_image_url = urljoin(str(base_url), str(product_image['src']))
 
-    #
 
-    #Print function 
-        print(
-        td_product_upc_text,
-        product_title,
-        th_product_price_incl_taxes.get_text(), td_product_price_incl_taxes.get_text(),
-        th_product_price_excl_taxes.get_text(), td_product_price_excl_taxes.get_text(),
-        product_stock_quantity,
-        product_description.get_text(),
-        td_product_category,
-        product_rating_value,
-        product_image_url)
+        #Push it in a list
+        book_data.append(td_product_upc_text)
+        book_data.append(product_title)
+        # book_data.append(th_product_price_incl_taxes.get_text())
+        book_data.append(td_product_price_incl_taxes)
+        # book_data.append(th_product_price_excl_taxes.get_text())
+        book_data.append(td_product_price_excl_taxes.get_text())
+        book_data.append(product_stock_quantity)
+        book_data.append(product_description.get_text())
+        book_data.append(td_product_category.get_text())
+        book_data.append(product_rating_value)
+        book_data.append(product_image_url)
+        book_data.append(url)
+
+        #Print function 
+        print(book_data)
+
+    else :
+        print('404')
+            
+
+#Get datas of every books on a category page 
+def get_datas_of_category_page(): 
+    books_data = []
+    for i in articles_links : 
+        print(i)
+        print(get_book_informations(i)) 
+        
+
+test = get_book_informations('https://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html')
+print(test)
 
 # CSV generation 
 def create_csv_file():
