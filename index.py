26e139d22441d413172ee7570624dd7f6ec5e1de
Edwin LAURENT <laurent.edwin@icloud.com>
2023-02-27 12:12:59 +0100

    Scraping datas of an entiere category OK

--- index.py
+++ index.py
@@ -6,13 +6,17 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 #GET url request 
-# url = 'http://books.toscrape.com/'
+def get_soup(url) : 
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup
 
-# response =requests.get(url)
-# soup = BeautifulSoup(response.text, 'html.parser')
+print('Starting scraping...')
 
 #Get href from category list (from left vertical menu)
 def get_categories_links() :
+    print('Collecting categories links...')
+    soup = get_soup('https://books.toscrape.com/index.html')
     categories_list = soup.select('ul.nav-list li ul li')
     categories_link_list = []
     for li in categories_list:
@@ -21,8 +25,11 @@ def get_categories_links() :
             href = link.get('href')
             href = href.replace('index.html','')
             categories_link_list.append(href)
+    print('Categories links collected')
     return categories_link_list
-#categories_list = get_categories_links()
+categories_list = get_categories_links()
+
+
 
 
 #Get all the pages of a specific category
@@ -48,6 +55,7 @@ def get_categories_links() :
 
 ### V2  for a single category
 def get_category_pages(category_path): 
+    print('Collecting category pages...')
     specific_category_pages = []
     page = 1    
     while True : 
@@ -61,19 +69,20 @@ def get_category_pages(category_path):
         else:
             page = 1
             break
+    print('Category pages collected')
     return specific_category_pages
 category_pages = get_category_pages('catalogue/category/books/historical-fiction_4/')
+
 
 
 
 #Get all articles links from category pages
 def get_articles_links() : 
     articles_link_list = []
-
+    print('Collecting articles links from category pages...')
     for i in category_pages : 
         url = i
-        response =requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = get_soup(url)
         articles_list = soup.select('div section div ol.row li article h3') 
 
         for li in articles_list:
@@ -85,6 +94,7 @@ def get_articles_links() :
                 href = href.replace('../../../','')
                 full_article_link = urljoin(('https://books.toscrape.com/catalogue/'), str(href))
                 articles_link_list.append(full_article_link)
+    print('Articles links collected')
     return articles_link_list
 articles_links = get_articles_links()
 
@@ -93,9 +103,10 @@ articles_links = get_articles_links()
 
 def get_book_informations(book_url):
     url = book_url
-    response =requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = get_soup(book_url)
     book_data = []
+    response = requests.get(url)
+    print('Collecting each book informations...')
     if response.ok : 
         article = soup.find('article')
         #Title
@@ -105,15 +116,15 @@ def get_book_informations(book_url):
         page_url = response.url
 
         #Price including taxes
-        th_product_price_incl_taxes = soup.find('th', text ='Price (incl. tax)')
+        th_product_price_incl_taxes = soup.find('th', string ='Price (incl. tax)')
         td_product_price_incl_taxes = th_product_price_incl_taxes.find_next_sibling('td')
 
         #Price excluding taxes 
-        th_product_price_excl_taxes = soup.find('th', text ='Price (excl. tax)')
+        th_product_price_excl_taxes = soup.find('th', string ='Price (excl. tax)')
         td_product_price_excl_taxes = th_product_price_excl_taxes.find_next_sibling('td')
 
         #Number available
-        th_product_availability = soup.find('th', text ='Availability')
+        th_product_availability = soup.find('th', string ='Availability')
         td_product_stock = th_product_availability.find_next_sibling('td').get_text()
         product_stock_quantity = int(re.findall(r'\d+', td_product_stock)[0])
 
@@ -122,7 +133,7 @@ def get_book_informations(book_url):
         product_description = product_description_title.find_next_sibling('p')
 
         #Category
-        th_product_type = soup.find('th', text ='Product Type')
+        th_product_type = soup.find('th', string ='Product Type')
         td_product_category = th_product_type.find_next_sibling('td')
 
         #Review rating
@@ -141,9 +152,9 @@ def get_book_informations(book_url):
         product_rating_value = help_dict[product_rating_value_string] 
 
         #UPC
-        th_product_upc = soup.find('th', text ='UPC')
+        th_product_upc = soup.find('th', string ='UPC')
         td_product_upc = th_product_upc.find_next_sibling('td')
-        th_product_upc_text = soup.find('th', text ='UPC').get_text()
+        th_product_upc_text = soup.find('th', string ='UPC').get_text()
         td_product_upc_text = th_product_upc.find_next_sibling('td').get_text()
         #Image URL 
 
@@ -169,52 +180,40 @@ def get_book_informations(book_url):
         book_data.append(url)
 
         #Print function 
-        print(book_data)
-
+        return book_data
     else :
-        print('404')
-            
+        print('Data not found for this book :' + book_url)
 
-#Get datas of every books on a category page 
-def get_datas_of_category_page(): 
-    books_data = []
-    for i in articles_links : 
-        print(i)
-        print(get_book_informations(i)) 
-        
+    print('Books informations collected')
 
-test = get_book_informations('https://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html')
-print(test)
+
+
+
 
 # CSV generation 
 def create_csv_file():
+    print('CSV generation...')
     #header for column names
-    header = ['page_url', 'upc',
-            'book_title',
-            'price_incl_taxes',
-            'price_excl_taxes',
-            'stock_quantity',
-            'description',
-            'category',
-            'rating',
-            'image_url'
+    header = ['UPC',
+            'Title',
+            'Price incl. taxes',
+            'Price excl. taxes',
+            'Stock quantity',
+            'Description',
+            'Category',
+            'Rating',
+            'Image URL',
+            'Article URL'
             ]
-    #Datas in file 
-    data =  [page_url,
-            td_product_upc_text,
-            product_title,
-            td_product_price_incl_taxes.get_text(),
-            td_product_price_excl_taxes.get_text(),
-            product_stock_quantity,
-            product_description.get_text(),
-            td_product_category.get_text(),
-            product_rating_value,
-            product_image_url]
-
     # create csv file / enter in writing mode
     with open('onlinebooks_scrapping.csv', 'w', newline='') as csvfile :
         writer = csv.writer(csvfile, delimiter=',')
         writer.writerow(header)
-        writer.writerow(data)
+        for i in articles_links : 
+                print(i)
+                writer.writerow(get_book_informations(i))
+    print('CSV generated')
 
+create_csv_file()
 
+print('End of the program')
